67f0e495a7881501bbdf9ea910a794e6976b270d
kitrasta <jaykick12@gmail.com>
2026-08-31 08:12:48 +0300

    feat: обновить страницу входа, добавить обработку состояния и отправку формы

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,24 +1,40 @@
 import style from './LoginPage.module.css';
+import { useState } from 'react';
 
 const LoginPage = () => {
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+    }
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('')
+
+
     return (
         <div className={style.wrapper}>
             <div className={style.container}>
-            <h2 className={style.title}>Capsa</h2>
+                <h2 className={style.title}>Capsa</h2>
 
-                <form className={style.form}>
-                        <label htmlFor='login'>Login</label>
+                <form
+                    className={style.form} onSubmit={handleSubmit}>
+                    <label htmlFor='login'>Login</label>
                     <input className={style.input}
+                        value={login}
+                        onChange={(e) => setLogin(e.target.value)}
                         type="text"
                         id='login'
                         placeholder='send your login' />
 
-                        <label htmlFor='password'>Password</label>
+                    <label htmlFor='password'>Password</label>
                     <input className={style.input}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         type="password"
                         id='password'
                         placeholder="send your password" />
-                        <button className={style.login} type="submit">Войти</button>
+                    <button className={style.login} type="submit">Войти</button>
                 </form>
 
             </div>
