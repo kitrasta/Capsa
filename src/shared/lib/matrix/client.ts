ec3f1963f6e6d974ed18bd5d1479207d0aa19062
kitrasta <jaykick12@gmail.com>
2026-08-31 18:14:03 +0300

    feat: улучшить форматирование кода и исправить опечатки в функции входа

--- src/shared/lib/matrix/client.ts
+++ src/shared/lib/matrix/client.ts
@@ -1,19 +1,16 @@
-import {createClient} from 'matrix-js-sdk';
+import { createClient } from 'matrix-js-sdk';
 
+export const loginUser = (login: string, password: string) => {
+  const client = createClient({ baseUrl: 'https://matrix.org' });
 
-
-export const loginUser = (login: string, password:string) => {
-const client = createClient({
-    baseUrl: 'https://matrix.org',
-});
-return client.loginRequest({
+  return client.loginRequest({
     type: 'm.login.password',
-    identifer: {
-        type: 'm.user',
-        user: 'login'
+    identifier: {
+      type: 'm.id.user',
+      user: login,
     },
     password,
-});
+  });
 };
 
 
