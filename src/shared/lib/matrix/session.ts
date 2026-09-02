949e0ba060635d62f782c1f46a659a9dba9bc0a3
kitrasta <jaykick12@gmail.com>
2026-09-02 16:14:56 +0300

    интерфейс MatrixSession и константы для управления сессией

---
+++ src/shared/lib/matrix/session.ts
@@ -0,0 +1,9 @@
+export interface MatrixSession {
+    accessToken: string,
+    userId: string,
+    deviceId: string,
+}
+
+const ACCESS_TOKEN_KEY = 'matrix_access_token';
+const USER_ID = '';
+const DEVICE_ID = '';
