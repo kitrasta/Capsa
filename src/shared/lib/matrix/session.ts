25a7bab23670d296781e709f71d49ea762d7d55c
kitrasta <jaykick12@gmail.com>
2026-09-02 19:59:22 +0300

    рефакторинг функции getSession для улучшения читаемости и упрощения логики

--- src/shared/lib/matrix/session.ts
+++ src/shared/lib/matrix/session.ts
@@ -8,17 +8,23 @@ const ACCESS_TOKEN_KEY = 'matrix_access_token';
 const USER_ID = 'matrix_user_id';
 const DEVICE_ID = 'matrix_device_id';
 
-export function getSession(accessToken: string, userId: string, deviceId: string):MatrixSession | null {
-    if (!localStorage.getItem(ACCESS_TOKEN_KEY) ||
-        !localStorage.getItem(USER_ID) ||
-        !localStorage.getItem(DEVICE_ID)) {
-            return null;
-        } 
-        return {accessToken, userId, deviceId} as MatrixSession
-}
+export function getSession(): MatrixSession | null {
+   const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+   const userId = localStorage.getItem(USER_ID);
+   const deviceId = localStorage.getItem(DEVICE_ID);
 
-export function saveSession(session: MatrixSession): void {
-    localStorage.setItem(ACCESS_TOKEN_KEY, session.accessToken);
-    localStorage.setItem(USER_ID, session.userId);
-    localStorage.setItem(DEVICE_ID, session.deviceId);
-}
+   if (!accessToken || !userId || !deviceId) {
+    return null;
+   } else {
+    return { accessToken, userId, deviceId };
+   }
+
+
+};
+
+export function saveSession(accessToken: string, userId: string, deviceId: string): MatrixSession | null {
+    localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+    localStorage.setItem(USER_ID, userId);
+    localStorage.setItem(DEVICE_ID,deviceId);
+    return { accessToken, userId, deviceId };
+};
