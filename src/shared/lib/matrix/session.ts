35ee8b5683f7252f9e9977fcca8401ac29b75732
kitrasta <jaykick12@gmail.com>
2026-09-02 19:28:38 +0300

    getSession для корректного получения сессии

--- src/shared/lib/matrix/session.ts
+++ src/shared/lib/matrix/session.ts
@@ -8,15 +8,13 @@ const ACCESS_TOKEN_KEY = 'matrix_access_token';
 const USER_ID = 'matrix_user_id';
 const DEVICE_ID = 'matrix_device_id';
 
-export function getSession():MatrixSession | null {
+export function getSession(accessToken: string, userId: string, deviceId: string):MatrixSession | null {
     if (!localStorage.getItem(ACCESS_TOKEN_KEY) ||
         !localStorage.getItem(USER_ID) ||
-        !localStorage.getItem(DEVICE_ID) === null) {
+        !localStorage.getItem(DEVICE_ID)) {
             return null;
-        } else {
-            return saveSession()
-        }
-
+        } 
+        return {accessToken, userId, deviceId} as MatrixSession
 }
 
 export function saveSession(session: MatrixSession): void {
