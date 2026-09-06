402877c10d0951d5d834a8303234ea73202e6e2e
kitrasta <jaykick12@gmail.com>
2026-09-06 16:33:20 +0300

    обновление функции loginUser для поддержки асинхронного выполнения и возвращения типа LoginResponse; добавление строгой типизации в tsconfig.app.json

--- src/shared/lib/matrix/client.ts
+++ src/shared/lib/matrix/client.ts
@@ -1,7 +1,11 @@
 import { createClient } from 'matrix-js-sdk';
+import type {LoginResponse} from "matrix-js-sdk";
 const client = createClient({ baseUrl: 'https://matrix.org' });
 
-export const loginUser = (login: string, password: string) => {
+export const loginUser = async (
+    login: string,
+    password: string,
+): Promise<LoginResponse> => {
 
 
     return client.loginRequest({
