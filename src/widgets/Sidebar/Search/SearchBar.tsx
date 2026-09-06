25c8aab464cebec7875db357aeae3a226702a3c1
kitrasta <jaykick12@gmail.com>
2026-09-06 21:13:58 +0300

    добавление отображения результатов поиска в компоненте SearchBar и строгая типизация для состояния результатов

--- src/widgets/Sidebar/Search/SearchBar.tsx
+++ src/widgets/Sidebar/Search/SearchBar.tsx
@@ -2,10 +2,11 @@ import styles from './SearchBar.module.css';
 import { useState, useEffect } from 'react';
 import { Search } from 'lucide-react';
 import { searchPublicRooms } from '../../../shared/lib/matrix/client';
+import type { IPublicRoomsChunkRoom } from 'matrix-js-sdk';
 
 const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState<IPublicRoomsChunkRoom[]>([]);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -39,6 +40,14 @@ const SearchBar = () => {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
+            {results.map((room) => (
+                <div key={room.room_id} className={styles.result}>
+                    <span>{room.name}</span>
+                    <span className={styles.members}>
+                        {room.num_joined_members}
+                    </span>
+                </div>
+            ))}
        
         </div>
 
