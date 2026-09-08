362712f122c224f297220837dab2c54ebe1ba249
kitrasta <jaykick12@gmail.com>
2026-09-08 12:41:35 +0300

    перенос логики поиска из компонента SearchBar в Sidebar для улучшения структуры кода

--- src/widgets/Sidebar/Search/SearchBar.tsx
+++ src/widgets/Sidebar/Search/SearchBar.tsx
@@ -5,27 +5,9 @@ import { searchPublicRooms } from '../../../shared/lib/matrix/client';
 import type { IPublicRoomsChunkRoom } from 'matrix-js-sdk';
 
 const SearchBar = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState<IPublicRoomsChunkRoom[]>([]);
-    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        if (searchTerm === '') return;
-        
-        const timer = setTimeout(async () => {
-            setLoading(true);
-            try {
-                const rooms = await searchPublicRooms(searchTerm);
-                setResults(rooms);
-            } catch (error) {
-                console.error('Error searching public rooms:', error);
-            } finally {
-                setLoading(false);
-            }
-        }, 500);
 
-        return () => clearTimeout(timer);
-    }, [searchTerm]);
+
 
     return (
         <div className={styles.wrapper}>
--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -3,6 +3,29 @@ import  Navbar  from './Navbar/Navbar';
 import SearchBar from './Search/SearchBar';
 
  const Sidebar = () => {
+
+        const [searchTerm, setSearchTerm] = useState('');
+        const [results, setResults] = useState<IPublicRoomsChunkRoom[]>([]);
+        const [loading, setLoading] = useState(false);
+
+            useEffect(() => {
+        if (searchTerm === '') return;
+        
+        const timer = setTimeout(async () => {
+            setLoading(true);
+            try {
+                const rooms = await searchPublicRooms(searchTerm);
+                setResults(rooms);
+            } catch (error) {
+                console.error('Error searching public rooms:', error);
+            } finally {
+                setLoading(false);
+            }
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [searchTerm]);
+
     return (
         <div className={styles.wrapper}>
             <h3>Capsa</h3>
